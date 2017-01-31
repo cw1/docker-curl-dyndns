059ee4622e8e4223486c6566e93e4266eea83c41
cw1 <c.witt.1900@gmail.com>
2017-01-31 14:28:00 +0100

    Add precise output and ip change tracking

--- startup.sh
+++ startup.sh
@@ -2,17 +2,24 @@
 
 # Check if DYN_DNS_URL is set
 if [ -z "$DYN_DNS_URL" ]; then
+  echo "Variable DYN_DNS_URL has to be set!"
   exit 1
 fi
 
 # Check if INTERVAL_TIME is set
 if [ -z "$INTERVAL_TIME" ]; then
+  echo "Variable INTERVAL_TIME is set to 300"
   INTERVAL_TIME=300
 fi
 
+old_ip=""
 # Infinite loop
-while :
-do
-  curl $CURL_PARAM $DYN_DNS_URL
+while true; do
+  current_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+  if ! [ "$current_ip" = "$old_ip" ]; then
+    curl -s $CURL_PARAM $DYN_DNS_URL > /dev/null
+    old_ip="$current_ip"
+    echo -n "DynDNS is updated to $current_ip on "; date
+  fi
   sleep $INTERVAL_TIME
 done
